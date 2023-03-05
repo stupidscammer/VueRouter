fa683133b96d5cb9e0d41485d923c1f6f5dc2b42
lucalysoft <lucaly17@gmail.com>
2023-03-05 01:22:20 -0800

    router example

---
+++ src/App.vue
@@ -0,0 +1,47 @@
+<template>
+  <ul class="nav">
+    <li>
+      <router-link to="/">Home</router-link>
+    </li>
+    <li>
+      <router-link to="/about">About</router-link>
+    </li>
+  </ul>
+  <router-view></router-view>
+</template>
+
+<script>
+export default {
+  name: "App",
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+}
+
+.router-link-active {
+  color: orange;
+}
+
+.router-link-exact-active {
+  color: crimson;
+}
+</style>
+
+<style scoped>
+ul {
+  display: flex;
+  list-style: none;
+  padding: 0;
+  margin: 0;
+}
+
+li:not(:last-of-type) {
+  margin-right: 1rem;
+}
+</style>
---
+++ src/router/index.js
@@ -0,0 +1,22 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import HomeView from '../views/Home.vue'
+import AboutView from '../views/About.vue'
+
+export const router = createRouter({
+  history: createWebHistory(),
+  routes: [
+    {
+      path: '/',
+      name: 'home',
+      component: HomeView
+    },
+    {
+      path: '/about',
+      name: 'about',
+      // route level code-splitting
+      // this generates a separate chunk (About.[hash].js) for this route
+      // which is lazy-loaded when the route is visited.
+      component: AboutView
+    }
+  ]
+})
